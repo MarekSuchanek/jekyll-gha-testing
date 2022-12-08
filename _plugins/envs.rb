4ca0e5c96ff5fe1c42de4e5ffa032b66c8fae4cc
Marek Suchánek <marek.suchanek@gmail.com>
2022-12-08 09:19:48 +0100

    Fix URLs variables

--- _plugins/envs.rb
+++ _plugins/envs.rb
@@ -2,9 +2,11 @@ module Jekyll
     class EnvironmentVariablesGenerator < Generator
       def generate(site)
         site.config['github_repo'] = ENV['GITHUB_REPO'] || 'unknown'
+        site.config['github_repo_url'] = ENV['GITHUB_REPO_URL'] || 'unknown'
         site.config['github_branch'] = ENV['GITHUB_BRANCH'] || 'unknown'
-        site.config['github_sha'] = ENV['GITHUB_SHA'] || 'unknown'
-        site.config['github_url'] = ENV['GITHUB_URL'] || 'unknown'
+        site.config['github_branch_url'] = ENV['GITHUB_BRANCH_URL'] || 'unknown'
+        site.config['github_commit'] = ENV['GITHUB_COMMIT'] || 'unknown'
+        site.config['github_commit_url'] = ENV['GITHUB_COMMIT_URL'] || 'unknown'
       end
     end
 end
